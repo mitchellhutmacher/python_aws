c181defe9c4fe13f8dfc44d80d40f5baa0b57944
Mitchell Hutmacher <mitchellhutmacher@Mitchells-MacBook-Pro.local>
2020-12-08 10:45:26 -0500

    Restored aws_rnw.py to a previous version

--- aws_rnw.py
+++ aws_rnw.py
@@ -1,26 +1,4 @@
 import json
-import logging
-import uuid
-
-import boto3
-from botocore.exceptions import ClientError
-
-logger = logging.getLogger(__name__)
-
-s3_resource = boto3.resource("s3")
-"""
-https://docs.aws.amazon.com/code-samples/latest/catalog/
-python-s3-s3_basics-bucket_wrapper.py.html
-"""
-
-def get_s3(region=None):
-    """Get a Boto 3 S3 resource with a specific Region or with your default Region."""
-    global s3_resource
-    if not region or s3_resource.meta.client.meta.region_name == region:
-        return s3_resource
-    else:
-        return boto3.resource('s3', region_name=region)
-
 
 if __name__ == "__main__":
     dict = {}
