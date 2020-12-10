0cfc652152a7d2a41f3cd3b6b88eade4c786ebfb
Mitchell Hutmacher <mitchellhutmacher@Mitchells-MacBook-Pro.local>
2020-12-09 19:05:58 -0500

    Made python file for front end

---
+++ front_end.py
@@ -0,0 +1,14 @@
+import aws_rnw as rnw
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog
+from PIL import Image, ImageTk
+
+
+def design_window():
+    my_font = ("arial", 18)
+
+    root = tk.Tk()
+
+    root.title("Client Feedback")
+    return
